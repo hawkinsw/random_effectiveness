5950b98c4073edb5a5347248bd25f4745ae09e8d
Will Hawkins <whh8b@virginia.edu>
2018-10-03 20:15:53 +0000

    Update scanner to capture connection statistics, too.

--- scanner.go
+++ scanner.go
@@ -4,12 +4,15 @@ import "net"
 import "fmt"
 import "time"
 import "ipgenerator"
+import "github.com/gonum/stat"
 
 var debug int = 1
 
 type scan_stats struct {
 	Success int
 	Failure int
+	AvgTime float64
+	StdDevTime float64
 }
 type runner_controller struct {}
 
@@ -24,19 +27,25 @@ func LocalhostIP() net.IPAddr {
 func runner(generator ipgenerator.IPGenerator, seed net.IPAddr, results chan scan_stats, controller chan runner_controller) {
 	success := 0
 	failure := 0
+	times := []float64{}
 	for {
 		select {
 			case <-controller:
 				if debug > 0 {
 					fmt.Printf("Stopping this runner.")
 				}
-				results<-scan_stats{success, failure}
+				results<-scan_stats{success,
+				                    failure,
+						    stat.Mean(times, nil),
+						    stat.StdDev(times, nil)}
 				return
 			default:
 				break
 		}
 		new_ip := generator.IP(seed, true)
-		if connect(new_ip, 80) {
+		connected, time := connect(new_ip, 445)
+		times = append(times, float64(time.Nanoseconds()))
+		if connected {
 			if debug > 0 {
 				fmt.Printf("Trying %v: Success.\n", new_ip);
 			}
@@ -55,23 +64,33 @@ func runner(generator ipgenerator.IPGenerator, seed net.IPAddr, results chan sca
  * and return true/false based on whether the connection
  * was successful.
  */
-func connect(target net.IPAddr, port uint16) bool {
+func connect(target net.IPAddr, port uint16) (bool, time.Duration) {
 	address := target.IP.String() + ":" + fmt.Sprint(port)
 	var conn net.Conn
 	var err error
+	var start time.Time
+	var elapsed time.Duration
+	var success bool
 
-	if conn, err = net.DialTimeout("tcp", address, 1*time.Second); err != nil {
-		return false
+	start = time.Now()
+	conn, err = net.DialTimeout("tcp", address, 1*time.Second)
+	elapsed = time.Now().Sub(start)
+
+	if err != nil {
+		success = false
+	} else {
+		success = true
+		conn.Close()
 	}
-	conn.Close()
-	return true
+
+	return success, elapsed
 }
 
 func main() {
 	executive := make(chan runner_controller)
 	results := make(chan scan_stats)
 	generator := ipgenerator.NewRandomIPGenerator()
-	total_runners := 2
+	total_runners := 1
 
 	/*
 	 * Create runners that will collect statistics.
@@ -84,7 +103,7 @@ func main() {
 		/*
 		 * Let us run for a period of time.
 		 */
-		time.Sleep(10*time.Second)
+		time.Sleep(60*time.Minute)
 		close(executive)
 	}()
 
@@ -94,7 +113,7 @@ func main() {
 		scan_stats := <-results
 		successes += scan_stats.Success
 		failures += scan_stats.Failure
-		fmt.Printf("Success: %v, failure: %v\n", scan_stats.Success, scan_stats.Failure)
+		fmt.Printf("Success: %v, failure: %v, average connect time: %v, std dev connect time: %v\n", scan_stats.Success, scan_stats.Failure, scan_stats.AvgTime, scan_stats.StdDevTime)
 	}
 	fmt.Printf("Total Successes: %v, Total Failures: %v\n", successes, failures)
 }
